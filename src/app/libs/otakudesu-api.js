275c202866d547c0878ed65b952e9210c4af9960
Muhammad Andreas Athallah Saifa Anam <andreasnation970@gmail.com>
2026-04-04 19:35:32 +0700

    chore: switch default otakudesu api to cloudflare cache worker

--- src/app/libs/otakudesu-api.js
+++ src/app/libs/otakudesu-api.js
@@ -1,4 +1,4 @@
-const DEFAULT_OTAKUDESU_API_URL = 'https://api-otakudesu-zeta.vercel.app';
+const DEFAULT_OTAKUDESU_API_URL = 'https://otaku-cache-worker.andreasnation970.workers.dev';
 
 export function getOtakudesuApiUrl() {
   return (process.env.NEXT_PUBLIC_API_URL || DEFAULT_OTAKUDESU_API_URL).replace(/\/+$/, '');
